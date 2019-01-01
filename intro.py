3e24fbdd2fa4f7214e99792488aacc204ee5f823
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2019-01-01 21:48:10 +0100

    day1&2 of 100daysofcode

---
+++ intro.py
@@ -0,0 +1,21 @@
+firstname = "Olayinka"
+lastname = "Idumu"
+Age = "28 year"
+Address = "No 8, Aduralere quaters of ijoka road Akure"
+State_of_Origin = " Ose L.G"
+Year_of_birth = "1990"
+Nationality = "Nigeria"
+Marital_status = "Single"
+Religion = "Christainity"
+
+print("Biodata Form")
+print("Firstname:",firstname)
+print("Lastname:" ,lastname)
+print("Age:",Age)
+print("Address:",Address)
+print("State of Origin",State_of_Origin)
+print("Year of birth:",Year_of_birth)
+print("Nationionality:",Nationality)
+print("Religion:",Religion)
+print("Marital Status",Marital_status)
+
---
+++ operator.py
@@ -0,0 +1,5 @@
+theta = 60
+pie = 3.142
+r = 3.5
+Area_of_a_sector = theta * pie * r
+print (Area_of_a_sector)
