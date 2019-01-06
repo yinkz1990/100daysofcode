b73a61e55f31fca7019ec4dfeebb400936de0245
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2019-01-06 21:57:23 +0100

    day 7 of 100days of code

---
+++ range.py
@@ -0,0 +1,14 @@
+n = int(input("max range ="))
+
+for i in range(1, n):
+    print(i)
+for i in range(1,n+1):
+    if(i%2==0):
+        print("even int", i)
+for i in range(n+1):
+    if(i%2==1):
+        print("odd int", i)
+
+ 
+
+    
