1cfa487c7dda63d6cec070ff0e2ac7456cf9d84f
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2019-01-08 19:55:33 +0100

    day 8 of 100days of code

--- voucher.py
+++ voucher.py
@@ -1,21 +1,20 @@
 import random
-n= ['Mtn','Glo','Airtel','Etisalat']
-network = int(input("Choose a network provider, 1 for Mtn, 2 for Glo, 3 for Airtel, 4 for Etislat"))
+network = int(input("Choose a network provider, 1. for Mtn, 2 for Glo, 3 for Airtel, 4 for Etislat : "))
 
-if (network == 0):
+if (network == 1):
     print("Welcome to Mtn service provider")
     for x in range(13):
         print (random.randrange(0, 10), end="")
-elif (network==1):
+elif (network==2):
     print("welcome to Glo service provider")
     for x in range(15):
         print(random.randrange(0, 10), end="")
 
-elif (network ==2):
+elif (network ==3):
     print("welcome to Airtel service provider")
     for x in range(13):
         print(random.randrange(0, 10) , end="") 
-elif (network ==3):
+elif (network ==4):
     print("welcome to Etisalat service provider")
     for x in range(16):
         print(random.randrange(0, 9), end="")
