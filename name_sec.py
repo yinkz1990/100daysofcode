563115844c97edc1d340578ab88c67e8746063f5
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2019-01-17 00:22:26 +0100

    modified day 14

--- name_sec.py
+++ name_sec.py
@@ -11,13 +11,14 @@ capture = []
 for i in range(len(python_students)):
     if python_students[i]==temp:
         capture.append(python_students[i+1])
-    if python_students[i]==temp:
         capture.append(python_students[i+2])
 capture_2=[]
 for i in capture:
     for j in i:
         capture_2.append(j)
-print(capture_2[1])
-print(capture_2[3])
-    
+if capture_2[1] == capture_2[3]:
+    print(capture_2[1])
+    print(capture_2[3])
+else:
+    print(capture_2[1])
 
